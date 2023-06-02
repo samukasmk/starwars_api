e379e51118a1f57d0c31bf6a7610d78652e7cfd5
Samuel Sampaio <samukasmk@gmail.com>
2023-06-02 12:42:15 -0300

    [feature/mongoengine-models] Initializing basic marshmallow schemas for serializations and deserializations

---
+++ starwars_api/rest_apis/starwars/schemas/planet.py
@@ -0,0 +1,7 @@
+from marshmallow_mongoengine import ModelSchema
+from starwars_api.models.starwars.planet import Planet
+
+
+class PlanetSchema(ModelSchema):
+    class Meta:
+        model = Planet
