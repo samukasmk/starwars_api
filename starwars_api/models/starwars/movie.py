d2563ffb48e015a75abc59204e42b0cd27048e69
Samuel Sampaio <samukasmk@gmail.com>
2023-06-02 12:35:47 -0300

    [feature/mongoengine-models] Initializing basic mongodb models without relationships

---
+++ starwars_api/models/starwars/movie.py
@@ -0,0 +1,14 @@
+import mongoengine
+
+
+class Movie(mongoengine.Document):
+    """StarWars Movie model object"""
+    title = mongoengine.StringField(required=True)
+    opening_crawl = mongoengine.StringField(required=True)
+    episode_id = mongoengine.IntField(required=True)
+    director = mongoengine.StringField(required=True)
+    producer = mongoengine.StringField(required=True)
+    release_date = mongoengine.DateField(required=True)
+
+    # TODO: move this: IntegerField to: mongoengine.ReferenceField(User, reverse_delete_rule=mongoengine.CASCADE)
+    planets = mongoengine.ListField(mongoengine.IntField())
---
+++ starwars_api/models/starwars/planet.py
@@ -0,0 +1,17 @@
+import mongoengine
+
+
+class Planet(mongoengine.Document):
+    """StarWars Planet model object"""
+    name = mongoengine.StringField(required=True)
+    rotation_period = mongoengine.StringField(required=True)
+    orbital_period = mongoengine.IntField(required=True)
+    diameter = mongoengine.IntField(required=True)
+    climate = mongoengine.StringField(required=True)
+    gravity = mongoengine.StringField(required=True)
+    terrain = mongoengine.StringField(required=True)
+    surface_water = mongoengine.IntField(required=True)
+    population = mongoengine.IntField(required=True)
+
+    # TODO: move this: IntegerField to: mongoengine.ReferenceField(User, reverse_delete_rule=mongoengine.CASCADE)
+    movies = mongoengine.ListField(mongoengine.IntField())
